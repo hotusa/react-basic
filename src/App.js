65213aa827c5141402017ea937f18e200f3a98ab
jmr <javier.marin@hotusa.com>
2020-03-05 16:10:58 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import React, {useContext, Suspense, lazy} from 'react';
-import './App.css'
-import {HashRouter, Redirect, Route, Switch} from "react-router-dom";
-import Navbar from "./components/Navbar";
-import {MainAppContext} from "./context";
-
-const Home = lazy(() => import('./pages/Home'))
-const Examples = lazy(() => import('./pages/Examples'))
+import React from 'react';
 
 function App() {
 
-    let {stateMainApp} = useContext(MainAppContext);
 
     return (
-        <div className={`theme-${stateMainApp.color}`}>
-            <HashRouter>
-                <Navbar/>
-                <hr/>
-                <Suspense fallback={""}>
-                    <Switch>
-                        <Route exact path={"/"} component={Home}/>
-                        <Route exact path={"/examples"} component={Examples}/>
-                        <Route path={"*"} render={() => <Redirect to='/404'/>}/>
-                    </Switch>
-                </Suspense>
-            </HashRouter>
+        <div>
+
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './i18n';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {HashRouter, Route, Switch} from "react-router-dom";
-import Login from "./pages/Login";
-import {MainAppProvider} from "./context";
-import NotFound from "./pages/NotFound";
 
 
 ReactDOM.render(
-    <MainAppProvider>
-        <Suspense fallback={""}>
-            <HashRouter>
-                <Switch>
-                    <Route exact path={"/login"} component={Login}/>
-                    <Route exact path={"/404"} component={NotFound}/>
-                    <Route path={"/"} component={App}/>
-                </Switch>
-            </HashRouter>
-        </Suspense>
-    </MainAppProvider>
-    , document.getElementById('root'));
+    <App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
